836e6c0d5d5a89a279a62907028441d046f1ec8a
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2022-01-06 15:26:36 -0300

    entendo mais sobre os dados, analisando resultados e qualidade dos algoritmos

--- 3 - Curso Machine learning com R Classificação de focos de dengue/Machine learning com R Classificação de focos de dengue.R
+++ 3 - Curso Machine learning com R Classificação de focos de dengue/Machine learning com R Classificação de focos de dengue.R
@@ -1,6 +1,6 @@
 library(DAAG)
+library(lattice)
 
-#data(dengue)
 names(dengue)
 summary(dengue)
 data(dengue)
@@ -16,3 +16,50 @@ ggplot(dengue, aes(x=humid, y=NoYes)) + geom_point() + geom_smooth(method="glm",
 
 hm<-data.frame(humid=23)
 predict(mb, hm, type='response')
+
+mb2 <-glm(NoYes~humid+temp, data=dengue, family=binomial)
+summary(mb2)
+
+hm2<- data.frame(humid=23,temp=34)
+
+predict(mb2, hm2, type='response')
+
+xyplot(temp~humid, data=dengue, groups=NoYes)
+
+set.seed(123)
+library(caret) #para modelo
+dengue2<-na.omit(dengue)
+
+dataindex<-createDataPartition(dengue2$NoYes,p=.7, list=FALSE)
+denguetreino<-dengue2[dataindex,] #separando treino
+dengueteste<-dengue2[-dataindex,] #separando teste
+
+maiscomum<-sum(denguetreino$NoYes==1)/(dim(denguetreino)[1])
+maiscomum
+
+if(maiscomum >= 0.5){
+  print("classeMaisComum <- 1")
+  classeMaisComum <-1
+}else{
+  print("classeMaisComum <- 0")
+  classeMaisComum <- 0
+}
+
+taxadeacerto<-sum(dengueteste$NoYes==classeMaisComum)/(dim(dengueteste)[1])
+taxadeacerto
+
+library(rattle)
+modelo<-train(as.factor(NoYes)~humid, data=denguetreino, method="rpart")
+fancyRpartPlot(modelo$finalModel)
+
+modelo2<-train(as.factor(NoYes)~temp, data=denguetreino, method="rpart")
+fancyRpartPlot(modelo2$finalModel)
+
+modelo3<-train(as.factor(NoYes)~trees, data=denguetreino, method="rpart")
+fancyRpartPlot(modelo3$finalModel)
+
+# modelo de classificação
+
+modeloml1<-train(as.factor(NoYes)~humid+temp+trees, data=denguetreino, metodh="glm")
+
+modeloml2<-train(as.factor(NoYes)~humid+temp+trees, data=denguetreino, metodh="glm")
